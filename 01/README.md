a5a51ef04b3e9debbf1dcbf8760404ebee08c693
Ester Rocha Soares <esterrochasoares123@gmail.com>
2022-09-16 20:01:03 -0300

    exerc11

---
+++ 01/README.md
@@ -0,0 +1,8 @@
+# EXERCICIO 01
+## CALCULANDO O IMC
+I = P / A**2
+I = IMC
+P = PESO
+A = ALTURA
+
+
---
+++ 01/exerc1.js
@@ -0,0 +1,5 @@
+const peso = 76;
+const altura = 1.79;
+const IMC = peso / altura ** 2;
+
+console.log(IMC);
