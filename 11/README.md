4be547368acf88ab7abe1ad5618394ef5b85e319
Ester Rocha Soares <esterrochasoares123@gmail.com>
2022-09-16 22:21:51 -0300

    exercicio11

---
+++ 11/README.md
@@ -0,0 +1,8 @@
+# EXERCICIO 11
+# volume de uma esfera
+V= 4/3 * PI* R **(3)
+
+V= volume da esfera
+D= diâmetro da esfera
+R= raio da esfera
+PI= Math.PI
---
+++ 11/exerc11.js
@@ -0,0 +1,5 @@
+const d = 6;
+const r = d / 2;
+const V = (4 / 3 * r ** (3));
+
+console.log(`O volume de uma esfera de raio è ${V}PI ou ${V * Math.PI}`);
